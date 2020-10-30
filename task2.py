3f9c125410c3978270b9125e05afe18758556f73
Kuseinova <sezimka.kuseinov@gmail.com>
2020-10-30 20:44:19 +0600

    second

---
+++ task2.py
@@ -0,0 +1,32 @@
+#1)
+#names = ['Alina','Aizhan','Adelina','Deana']
+#for name in names:
+  #  if name == "admin":
+ #       print("Hello admin,would you like to see a status report?")
+
+
+#2)
+#cars = ['BMW','Lamba','Porshe','Tayota']
+#massage = 'My first favorite car was a' +cars[0].title()+"."
+#print(massage)
+
+#3)
+#s = input('str1' , ' str2')
+#first_word = s[:s.find(' ')]
+#second_word = s[s.find(' ')+1:]
+#print(second_word + ' ' + fist_word)
+
+#4)s = input(1, 2, 3, 4, 5, 6, 7)
+#print(s[(len(s) + 1) // 2:] + s[:(len(s) + 1) // 2])
+
+#1)
+guests = ['Anne','Kate','Marina']
+
+print(Hello,guests.title() +"Come for lunch today.")
+
+#2)
+guests = ['Anne','Kate','Marina']
+print(guests[0:2])
+print(guest.title(2)+",can't come today")
+
+#3)
---
+++ task4.py
@@ -0,0 +1,39 @@
+# 1)Task
+
+# menu = {'Lagman' : 120, 'Plov' : 120, 'Borsh' : 100}
+# menu["Besh barmak"] = 130
+# menu["Lagman"] = 135
+# del menu['Borsh']
+# print(menu)
+ 
+
+ #2)Task
+#  slovar = {'hello' : 'hi', 'good' : 'nice', 'price' : 'cost'}
+#  c = (stovar['price'])
+# print(c)
+
+
+# #3)Task
+# gorod = {'Russia', 'USA', 'German'}
+# strana = {'New York', 'Berlin', 'Moscov'}
+#gorod_strana = dict(zip(gorod, strana)
+# print(gorod_strana)
+# gs = dict.fromkeys(['Russia', 'USA', 'German'], 'Moscow', 'New York', 'Berlin')
+# print(gs)
+
+
+
+# #4)Task
+# dog = {'name' : 'Bobik', 'color' : 'black', 'age' : 5}
+# cat = {'name': 'Barsik', 'color': 'write', 'age' : 3}
+# for key, value is dog.items():
+#     print("This is a dog . His " + str(key) + 'is' + str(value). 
+# "He is" +str(value) + "and he is already" + str(value) + 'years old')
+# for key, value is cat.items()  
+# print ('This is a cat . His' + str (key) + 'is' + str(value))   
+
+
+# #5) Task
+# a = {'1a' : ['John', 'Jessica', 'Tom'], 
+#  '2b' : ['Ben', 'Adam', 'Elena']}
+# print('This is Tom. He is in grade ' + [0])
