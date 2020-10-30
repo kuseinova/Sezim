674c0a0f8227a90324ad1f51efca6460d88bb464
Kuseinova <sezimka.kuseinov@gmail.com>
2020-10-30 20:04:54 +0600

    First commit

---
+++ task3.py
@@ -0,0 +1,40 @@
+#1
+# a = "helo"
+# print(len(a))
+
+# 2
+# a = "helo"
+# print(a.swapcase())
+# print(a.lower())
+
+# 3
+# a = 'hello'
+# a = '$' + a[1:]
+# a = a[0:4] + '$'
+# print(a)
+
+
+# 4
+# a = "hello world python bootcamp"
+# print(a.split())
+
+# 5
+# a = "hello"
+# a2 = "sezim"
+# print(a + ' ' + a2)\
+
+# # 6
+# a = "makers bootcamp evening"
+# print(len(a.split(' ')))
+
+# 7
+# a = "hello world!"
+# print(a[3])
+# print(a[10])
+# print(a[0:5])
+# print(a[0:10])
+# print(a[::2])
+# print(a[1::2])
+# print(a[::-1])
+# print(a[::-2])
+# print(len(a))
